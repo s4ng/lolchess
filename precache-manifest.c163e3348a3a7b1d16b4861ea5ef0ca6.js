5f87e1454aec58b04f7b510655b49b91d66a0985
s4ng <zalcls951201@gmail.com>
2019-08-17 22:07:10 +0900

    Updates

---
+++ precache-manifest.c163e3348a3a7b1d16b4861ea5ef0ca6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5065b1bb20516668886b84077eb4da23",
+    "url": "/lolchess/index.html"
+  },
+  {
+    "revision": "770b3259761bc9557771",
+    "url": "/lolchess/static/css/main.dabf41cd.chunk.css"
+  },
+  {
+    "revision": "8b44617518f277020a9c",
+    "url": "/lolchess/static/js/2.795458d1.chunk.js"
+  },
+  {
+    "revision": "770b3259761bc9557771",
+    "url": "/lolchess/static/js/main.ded08882.chunk.js"
+  },
+  {
+    "revision": "1f98cd96c7ae546e7d26",
+    "url": "/lolchess/static/js/runtime~main.48fc4445.js"
+  }
+]);
